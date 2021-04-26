6a37b3915d61cc1b4c1bebb4a529787946a99bdf
ekomikie <61963790+ekomikie@users.noreply.github.com>
2021-04-26 18:16:19 +0100

    added transformed navbar toggler

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,17 @@
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', ()=>{
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  }else{
+    menuBtn.classList.remove('open');
+    menuOpen = false; 
+  }
+})
+
+
 $('.owl-carousel').owlCarousel({
   loop:true,
   margin:20,
